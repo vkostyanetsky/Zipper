6f217dd65d459bff06897d6555a34109b8b63dcf
vkostyanetsky <vlad@kostyanetsky.ru>
2023-02-23 11:05:52 +0400

    Code polishing

--- zipper.py
+++ zipper.py
@@ -20,12 +20,14 @@ def make_backup(source_path: str, target_path: str) -> None:
     target_file_name = time.strftime("%Y_%m_%d %H_%M_%S.zip")
     target_file_path = os.path.join(target_path, target_file_name)
 
-    with zipfile.ZipFile(target_file_path, "w") as fantasy_zip:
+    with zipfile.ZipFile(target_file_path, "w") as target_zip_file:
         for folder, _, files in os.walk(source_path):
             for file in files:
-                fantasy_zip.write(
-                    os.path.join(folder, file),
-                    os.path.relpath(os.path.join(folder, file), source_path),
+                src_file_path = os.path.join(folder, file)
+                arc_file_path = os.path.relpath(os.path.join(folder, file), source_path)
+                target_zip_file.write(
+                    src_file_path,
+                    arc_file_path,
                     compress_type=zipfile.ZIP_DEFLATED,
                 )
 
